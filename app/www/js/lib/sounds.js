92933088b55e27eef9a5452918294e8da2bf56bc
Hynek Urban <WWuzzy@gmail.com>
2013-10-12 12:10:19 +0200

    Add the sounds module.

---
+++ app/www/js/lib/sounds.js
@@ -0,0 +1,28 @@
+/* TODO: port to Web Audio API */
+
+define(function() {
+
+	var output = new Audio();
+	output.mozSetup(1, 44100);
+
+	/* The short beep during countdown. */
+	var samples_countdown = new Float32Array(11025);
+	for (var i = 0; i < samples_countdown.length ; i++) {
+		samples_countdown[i] = Math.sin( i / 15 );
+	}
+
+	/* The long beep after end of game. */
+	var samples_time_expired = new Float32Array(44100);
+	for (var i = 0; i < samples_time_expired.length ; i++) {
+		samples_time_expired[i] = Math.sin( i / 15 );
+	}
+
+	return {
+		play_countdown: function() {
+			output.mozWriteAudio(samples_countdown);
+		},
+		play_time_expired: function() {
+			output.mozWriteAudio(samples_time_expired);
+		}
+	}
+});

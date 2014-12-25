7343437e28696da0ad776cbba40b6c14beb21497
Hynek Urban <WWuzzy@gmail.com>
2014-12-25 13:24:37 +0100

    Port the rest of the sounds module to Web Audio API.

--- app/www/js/lib/sounds.js
+++ app/www/js/lib/sounds.js
@@ -1,24 +1,36 @@
 /*jslint browser: true, plusplus: true, todo: true, nomen: true */
 
-/* TODO: port to Web Audio API */
-
-var define, Audio;
+var define;
 
 define(function () {
     "use strict";
     var
         i,
         context = new window.AudioContext(),
-        click_sound = new Audio("sounds/click.wav"),
         oscillator = context.createOscillator(),
-        gainNode = context.createGain();
+        gainNode = context.createGain(),
+        click_request = new XMLHttpRequest(),
+        click_sound_buffer;
 
 
+	// -- > Create and start the oscilator for beeping sounds.
 	oscillator.connect(gainNode);
 	// Gain and frequency values are empirical; there's nothing special about them.
 	gainNode.gain.value = 0.16;
 	oscillator.frequency.value = 940;
 	oscillator.start();
+	// < --
+
+	// -- > Prepare the buffer for click sounds.
+	click_request.open('GET', 'sounds/click.wav', true);
+	click_request.responseType = 'arraybuffer';
+	click_request.onload = function() {
+	context.decodeAudioData(click_request.response, function(buffer) {
+	    click_sound_buffer = buffer;
+	  });
+	}
+	click_request.send();
+	// < --
 
 	function beep(duration) {
 		gainNode.connect(context.destination);
@@ -33,7 +45,13 @@ define(function () {
 			beep(1000);
         },
         play_click: function () {
-			click_sound.play();
+			var click_buffer_source = context.createBufferSource();
+			// Don't play the sound before its buffer has been
+			// initialized.
+			if (click_sound_buffer === undefined) return;
+			click_buffer_source.buffer = click_sound_buffer;
+			click_buffer_source.connect(context.destination);
+			click_buffer_source.start();
         }
     };
 });

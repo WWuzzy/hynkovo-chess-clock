9d46fe61e5c717da222ffeff45f52b51cc80c7dd
Hynek Urban <WWuzzy@gmail.com>
2014-12-25 21:17:33 +0100

    Improve coding style to conform to the rules of JSLint.

--- app/www/js/lib/sounds.js
+++ app/www/js/lib/sounds.js
@@ -5,7 +5,6 @@ var define;
 define(function () {
     "use strict";
     var
-        i,
         context = new window.AudioContext(),
         oscillator = context.createOscillator(),
         gainNode = context.createGain(),
@@ -13,45 +12,47 @@ define(function () {
         click_sound_buffer;
 
 
-	// -- > Create and start the oscilator for beeping sounds.
-	oscillator.connect(gainNode);
-	// Gain and frequency values are empirical; there's nothing special about them.
-	gainNode.gain.value = 0.16;
-	oscillator.frequency.value = 940;
-	oscillator.start();
-	// < --
+    // -- > Create and start the oscilator for beeping sounds.
+    oscillator.connect(gainNode);
+    // Gain and frequency values are empirical; there's nothing special about them.
+    gainNode.gain.value = 0.16;
+    oscillator.frequency.value = 940;
+    oscillator.start();
+    // < --
 
-	// -- > Prepare the buffer for click sounds.
-	click_request.open('GET', 'sounds/click.wav', true);
-	click_request.responseType = 'arraybuffer';
-	click_request.onload = function() {
-	context.decodeAudioData(click_request.response, function(buffer) {
-	    click_sound_buffer = buffer;
-	  });
-	}
-	click_request.send();
-	// < --
+    // -- > Prepare the buffer for click sounds.
+    click_request.open('GET', 'sounds/click.wav', true);
+    click_request.responseType = 'arraybuffer';
+    click_request.onload = function () {
+        context.decodeAudioData(click_request.response, function (buffer) {
+            click_sound_buffer = buffer;
+        });
+    };
+    click_request.send();
+    // < --
 
-	function beep(duration) {
-		gainNode.connect(context.destination);
-		setTimeout(function () {gainNode.disconnect(context.destination);}, duration);
-	}
+    function beep(duration) {
+        gainNode.connect(context.destination);
+        setTimeout(function () {gainNode.disconnect(context.destination); }, duration);
+    }
 
     return {
         play_countdown: function () {
-			beep(250);
+            beep(250);
         },
         play_time_expired: function () {
-			beep(1000);
+            beep(1000);
         },
         play_click: function () {
-			var click_buffer_source = context.createBufferSource();
-			// Don't play the sound before its buffer has been
-			// initialized.
-			if (click_sound_buffer === undefined) return;
-			click_buffer_source.buffer = click_sound_buffer;
-			click_buffer_source.connect(context.destination);
-			click_buffer_source.start();
+            var click_buffer_source = context.createBufferSource();
+            // Don't play the sound before its buffer has been
+            // initialized.
+            if (click_sound_buffer === undefined) {
+                return;
+            }
+            click_buffer_source.buffer = click_sound_buffer;
+            click_buffer_source.connect(context.destination);
+            click_buffer_source.start();
         }
     };
 });
